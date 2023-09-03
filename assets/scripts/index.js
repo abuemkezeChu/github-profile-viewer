2f56d6d16b266fa54d6bb28f9b49e7641941db54
Abuemkeze Chu <abuemkeze@gmail.com>
2023-09-03 20:34:14 +0100

    fix: lint errors

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,29 @@
 'use strict'
 
-let search = document.getElementById('username')
+const search = document.getElementById('username')
 const button = document.getElementById('search')
 const profileCard = document.querySelector('.profile')
-const repoCard = document.getElementById('repos-list')
 
 // render profile card
 const renderProfile = (data) => {
-  // profile properties
-  const { avatar_url, html_url, login, bio, public_repos, followers, following } = data
-  
   const profile = `
       <div class="profile-image">
-        <img src="${avatar_url}" alt="profile picture" id="profile-image">
+        <img src="${data.avatar_url}" alt="profile picture" id="profile-image">
       </div>
       <div class="profile-info">
-        <h1 id="profile-name"><a href="${html_url}">${login}</a></h1>
-        <p id="profile-bio">${bio}</p>
+        <h1 id="profile-name"><a href="${data.html_url}">${data.login}</a></h1>
+        <p id="profile-bio">${data.bio}</p>
         <div class="profile-info-details">
           <div class="profile-info-details-item">
-            <span id="profile-repos">${public_repos}</span>
+            <span id="profile-repos">${data.public_repos}</span>
             <span>Repos</span>
           </div>
           <div class="profile-info-details-item">
-            <span id="profile-followers">${followers}</span>
+            <span id="profile-followers">${data.followers}</span>
             <span>Follower(s)</span>
           </div>
           <div class="profile-info-details-item">
-            <span id="profile-following">${following}</span>
+            <span id="profile-following">${data.following}</span>
             <span>Following</span>
           </div>
         </div>
@@ -46,7 +42,7 @@ const getUserData = (username) => {
     .catch(error => console.log(error))
 }
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
   const username = search.value
   if (username !== '') {
     getUserData(username)
